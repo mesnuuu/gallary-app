20ed305a7b40d77e2571e78a1295a44fb48afafe
Muhammad Saleem <46952459+mesnuuu@users.noreply.github.com>
2023-03-13 10:34:52 +0500

    Adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div class="container text-center mt-5">
 
-      
+    <h1>Gallary</h1>
+
     </div>
   );
 }
