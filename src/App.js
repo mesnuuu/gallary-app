d57c20ab1a9bc1834208efba703f71a0dca6db76
Muhammad Saleem <46952459+mesnuuu@users.noreply.github.com>
2023-03-13 17:31:51 +0500

    Creating reusable components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 
 import './App.css';
+import Card from './components/Card';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div class="container text-center mt-5">
 
-    <h1>Gallary</h1>
+    <>
 
-    </div>
+      <div class="container text-center mt-5">
+
+        <Navbar />
+
+        <h1>Gallary</h1>
+
+        <div className='row'>
+
+          {Array.apply(null, { length: 9 }).map(() => <Card />)}
+
+        </div>
+
+      </div>
+
+    </>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+
+        <div className='col mb-5'>
+            <div className="card" style={{ width: "18rem" }}>
+                <img src="https://via.placeholder.com/200" className="card-img-top" alt="..." />
+            </div>
+        </div>
+
+    )
+}
+
+export default Card
